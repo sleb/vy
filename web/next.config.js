a62c5ea8a61bc3ad2e02e932455d51ebfb26e308
sleb <scott.g.lebaron@gmail.com>
2025-08-24 20:30:37 -0700

    Switch to @tailwindcss/postcss and update deps

--- web/next.config.js
+++ web/next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   async rewrites() {
     return [
       {
-        source: '/api/vy/:path*',
-        destination: process.env.VY_API_URL ? `${process.env.VY_API_URL}/:path*` : 'http://localhost:3001/api/:path*',
+        source: "/api/vy/:path*",
+        destination: process.env.VY_API_URL
+          ? `${process.env.VY_API_URL}/:path*`
+          : "http://localhost:3001/api/:path*",
       },
     ];
   },
   env: {
-    VY_API_URL: process.env.VY_API_URL || 'http://localhost:3001',
+    VY_API_URL: process.env.VY_API_URL || "http://localhost:3001",
   },
 };
 
